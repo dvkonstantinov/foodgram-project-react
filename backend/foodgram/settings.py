360572a3e49f31a90684d131dfa4d72007d543c7
dimma <121demon@mail.ru>
2022-08-19 13:10:24 +0300

    Отключение режима debug

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='12345qwerty')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['backend', '127.0.0.1', 'foodgram.dvkonstantinov.ru']
 
